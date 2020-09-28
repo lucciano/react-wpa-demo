8af2223d5003d000ba3ffc1c345a4be454fcc04d
Luciano Andrade <luciano@miracledevs.com>
2020-09-28 16:10:16 -0300

    updated

--- config-overrides.js
+++ config-overrides.js
@@ -1,58 +1,40 @@
+var { GenerateSW } = require('workbox-webpack-plugin');
 const fs = require("fs")
-const path = require("path")
+var find = require('find');
+var path = require('path');
 const hasha = require('hasha');
 
-
 const hashaOptions = {
-	algorithm: "sha1"
+    algorithm: "sha1"
 }
 
 sha1FromFile = filepath => hasha.fromFileSync(filepath, hashaOptions);
 sha1FromString = string => hasha(string, hashaOptions);
 
-const getAllFiles = function (dirPath, arrayOfFiles)
-{
-    files = fs.readdirSync(dirPath)
-
-    arrayOfFiles = arrayOfFiles || []
-
-    files.forEach(function (file)
-    {
-        const fileName = path.join(dirPath, file);
-        if (fs.statSync(fileName).isDirectory()) {
-            arrayOfFiles = getAllFiles(fileName, arrayOfFiles)
-        } else {
-            arrayOfFiles.push(fileName)
-        }
-    })
-
-    return arrayOfFiles
-}
-
 module.exports = function override(config, env)
 {
     config.plugins.forEach((plugin) =>
     {
-        if (env === "production" && plugin.constructor.name === 'GenerateSW') {
+        if (env === "production" && plugin.constructor.name === GenerateSW.name) {
 
             const publicDirectoryPath = path.join(__dirname, 'public');
-
             const exclude = ['index.html', 'manifest.json', 'robots.txt', 'favicon.ico'];
-            const precache = getAllFiles(publicDirectoryPath).filter(file => !(exclude.includes(path.relative(publicDirectoryPath, file)))).map(file => 
+            find.fileSync(publicDirectoryPath).filter(file => file.match(/app-precache-[a-f0-9]+\.js/)).forEach(file => { fs.unlinkSync(file) })
+
+            const precache = find.fileSync(publicDirectoryPath).filter(file => !(exclude.includes(path.relative(publicDirectoryPath, file)))).map(file =>
             {
-                // console.log(path.relative(publicDirectoryPath, file), !(exclude.includes(path.relative(publicDirectoryPath, file))));
-                const url = '/' + path.relative(publicDirectoryPath, file).replace(/\\/g, '/'); 
+                const url = './' + path.relative(publicDirectoryPath, file).replace(/\\/g, '/');
                 const revision = sha1FromFile(file);
-                return {url, revision};
+                return { url, revision };
             });
             const appPrecacheJS = `self.__precacheManifest = (self.__precacheManifest || []).concat(${JSON.stringify(precache, null, 4)});`
-            getAllFiles(publicDirectoryPath).filter(file => file.match(/app-precache-[a-f0-9]+\.js/)).forEach(file => {fs.unlinkSync(file)})
+
             const contentHash = sha1FromString(appPrecacheJS);
             const fileName = `app-precache-${contentHash}.js`
             fs.writeFileSync(path.join(publicDirectoryPath, fileName), appPrecacheJS);
-            plugin.config.importScripts.push(`/${fileName}`);
+            plugin.config.importScripts.push(`/${fileName}`)
 
         }
-    })
+    });
     return config;
 }
